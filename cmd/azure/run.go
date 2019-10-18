1b6c3387983b745e21da057a733e8c810b74d02b
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-10-18 12:54:11 -0500

    Log using hclog to avoid blowing up plugin framework

--- cmd/azure/run.go
+++ cmd/azure/run.go
@@ -6,16 +6,11 @@ import (
 )
 
 func buildRunCommand(p *azure.Plugin) *cobra.Command {
-	var opts azure.RunOptions
-
 	cmd := &cobra.Command{
 		Use:   "run PLUGIN_IMPLEMENTATION",
 		Short: "Run the plugin and listen for client connections.",
-		PreRunE: func(cmd *cobra.Command, args []string) error {
-			return opts.Validate(args)
-		},
 		Run: func(cmd *cobra.Command, args []string) {
-			p.Run(opts)
+			p.Run(args)
 		},
 	}
 
--- pkg/azure/run.go
+++ pkg/azure/run.go
@@ -3,40 +3,55 @@ package azure
 import (
 	"strings"
 
+	"github.com/hashicorp/go-hclog"
 	"github.com/deislabs/porter/pkg/plugins"
 	"github.com/hashicorp/go-plugin"
 	"github.com/pkg/errors"
 )
 
 type RunOptions struct {
-	Implementation    string
+	Key               string
 	selectedPlugin    plugin.Plugin
 	selectedInterface string
 }
 
 func (o *RunOptions) Validate(args []string) error {
 	if len(args) == 0 {
-		return errors.New("The positional argument PLUGIN_IMPLEMENTATION was not specified")
+		return errors.New("The positional argument KEY was not specified")
 	}
 	if len(args) > 1 {
-		return errors.New("Multiple positional arguments were specified but only one, PLUGIN_IMPLEMENTATION is expected")
+		return errors.New("Multiple positional arguments were specified but only one, KEY is expected")
 	}
 
-	o.Implementation = args[0]
+	o.Key = args[0]
 
 	availableImplementations := GetAvailableImplementations()
-	selectedPlugin, ok := availableImplementations[o.Implementation]
+	selectedPlugin, ok := availableImplementations[o.Key]
 	if !ok {
-		return errors.Errorf("invalid PLUGIN_IMPLEMENTATION specified: %q", o.Implementation)
+		return errors.Errorf("invalid plugin key specified: %q", o.Key)
 	}
 	o.selectedPlugin = selectedPlugin()
 
-	parts := strings.Split(o.Implementation, ".")
+	parts := strings.Split(o.Key, ".")
 	o.selectedInterface = parts[0]
 
 	return nil
 }
 
-func (p *Plugin) Run(opts RunOptions) {
+func (p *Plugin) Run(args []string) {
+	// We are not following the normal CLI pattern here because
+	// if we write to stdout without the hclog, it will cause the plugin framework to blow up
+	var opts RunOptions
+	err := opts.Validate(args)
+	if err != nil {
+		logger := hclog.New(&hclog.LoggerOptions{
+			Name:   "azure",
+			Output: p.Err,
+			Level:  hclog.Error,
+		})
+		logger.Error(err.Error())
+		return
+	}
+
 	plugins.Serve(opts.selectedInterface, opts.selectedPlugin)
 }
