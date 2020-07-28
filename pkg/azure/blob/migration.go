442eecfa36cd7d52c3f9387cf56f2975d03724f5
Carolyn Van Slyck <me@carolynvanslyck.com>
2020-07-28 13:24:01 -0500

    Safe write tags
    Ensure that the tag is readable before returning. This works around a
    problem where there is an unknown delay after setting a tag on a blob
    before you can query for the blob by the tag. When we return
    immediately, this causes bugs because Porter expects to be able to query
    for the data immediately using the crud.Store interface which relies on
    the tags.
    
    So after setting a tag on a blob, query for it in a loop until we get
    the blob back, then allow the call to return.

--- pkg/azure/blob/migration.go
+++ pkg/azure/blob/migration.go
@@ -3,8 +3,8 @@ package blob
 import (
 	"context"
 
-	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
 )
 
 // Add tags to each claim so that when the migration runs,
@@ -12,29 +12,27 @@ import (
 // type = claims
 
 func (s *Store) migrateToTaggedClaims() error {
+	s.logger.Warn("Tagging claims data in preparation for claims migration")
+
 	// List all claims
 	blobs, err := s.listBlobs("claims/")
 	if err != nil {
-		return errors.Wrap(err, "unable to list claims for migration to the new azure plugin storage format")
+		return errors.Wrap(err, "Unable to list claims for migration to the new azure plugin storage format")
 	}
 
-	container, err := s.buildContainerURL()
-	if err != nil {
-		return err
+	tags := map[string]string{
+		"type": "claims",
 	}
 
-	var bigErr *multierror.Error
+	g := new(errgroup.Group)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	for _, blob := range blobs {
-		tags := map[string]string{
-			"type": "claims",
-		}
-
-		blobURL := container.NewBlobURL(blob.Name)
-		_, err = blobURL.SetTags(context.Background(), tags)
-		if err != nil {
-			err = multierror.Append(bigErr, err)
-		}
+		g.Go(func() error {
+			return s.setTag(ctx, blob.Name, tags)
+		})
 	}
 
-	return bigErr.ErrorOrNil()
+	return g.Wait()
 }
