581d19b1a5246a8b10eae89f75b6235a05c7933c
Steven Gettys <s.gettys@f5.com>
2022-12-08 16:32:16 -0800

    feat: Added ability to parse keyvalue as an azure keyvault ID to support lookups across multiple keyvaults
    Signed-off-by: Steven Gettys <s.gettys@f5.com>

--- pkg/azure/keyvault/store.go
+++ pkg/azure/keyvault/store.go
@@ -3,6 +3,7 @@ package keyvault
 import (
 	"context"
 	"fmt"
+	"net/url"
 	"strings"
 
 	"get.porter.sh/plugin/azure/pkg/azure/azureconfig"
@@ -19,6 +20,12 @@ var _ plugins.SecretsProtocol = &Store{}
 const (
 	SecretKeyName = "secret"
 )
+
+type secret struct {
+	vaultURL string
+	name     string
+	version  string
+}
 
 // Store implements the backing store for secrets in azure key vault.
 type Store struct {
@@ -65,7 +72,22 @@ func (s *Store) Resolve(ctx context.Context, keyName string, keyValue string) (s
 	if err := s.Connect(ctx); err != nil {
 		return "", err
 	}
-
+	// Check if the keyValue is set to a full ID or just the secret name. The keyValue is only considered
+	// an ID if all 3 pieces of info (keyvault URL, secret name, and version) parse, otherwise the configured vault URL is
+	// used and the keyValue is assumed to be the full secret name inside that vault
+	secret := parseID(ctx, keyValue)
+	if secret != nil {
+		result, err := s.client.GetSecret(ctx, secret.vaultURL, secret.name, secret.version)
+		if err != nil {
+			// Instead of return error in this case instead log as a debug and attempt to fetch
+			// the secret from the configured secret store. Only return error if the secret is unable
+			// to be resolved in both ways
+			log.Debug(fmt.Sprintf("could not get secret %s by ID: %s", keyValue, err.Error()))
+		} else {
+			// If we were able to look it up based off of the parsed ID then return that immediately
+			return *result.Value, nil
+		}
+	}
 	secretVersion := ""
 	result, err := s.client.GetSecret(ctx, s.vaultUrl, keyValue, secretVersion)
 	if err != nil {
@@ -95,6 +117,38 @@ func (s *Store) Create(ctx context.Context, keyName string, keyValue string, val
 	if err != nil {
 		return log.Error(fmt.Errorf("failed to set secret for key %s in azure-keyvault: %w", keyValue, err))
 	}
-
 	return nil
+}
+
+// parseID will attempt to create a secret from an id. If the id is not valid then
+// it will log a debug and return nil
+func parseID(ctx context.Context, id string) *secret {
+	_, log := tracing.StartSpan(ctx, attribute.String("parsing secret as ID", id))
+	if id == "" {
+		return nil
+	}
+	parsed, err := url.Parse(id)
+	if err != nil {
+		log.Debug(fmt.Sprintf("Unable to parse %s as secret ID: %s", id, err.Error()))
+		return nil
+	}
+	url := fmt.Sprintf("%s://%s", parsed.Scheme, parsed.Host)
+	// Trim preceeding and trailing slashes
+	split := strings.Split(strings.TrimSuffix(strings.TrimPrefix(parsed.Path, "/"), "/"), "/")
+	if len(split) < 3 {
+		if len(split) == 2 {
+			return &secret{
+				vaultURL: url,
+				name:     split[1],
+				version:  "",
+			}
+		}
+		log.Debug(fmt.Sprintf("Unexpected ID format found for %s, unable to parse as secret ID", id))
+		return nil
+	}
+	return &secret{
+		vaultURL: url,
+		name:     split[1],
+		version:  split[2],
+	}
 }
